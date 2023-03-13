4a71fa8b75f926ad2382637a178525305c8511d9
Benjamin Fian <benjamin.fian@gmail.com>
2023-03-13 15:40:08 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faEnvelope, faKey } from '@fortawesome/free-solid-svg-icons';
+// library.add(faEnvelope, faKey); //exemples
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; // take this in a component
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+      {/* <FontAwesomeIcon icon="envelope" />
+      <FontAwesomeIcon icon="key" /> */} 
+    </div>
+  )
+}
+
+export default App
